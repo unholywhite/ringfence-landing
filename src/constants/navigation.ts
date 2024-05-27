fcea9ee00ecdebaf6fa85d47ebbffccca969f618
AlexeyPonomarenko <a@a.com>
2024-05-27 23:02:52 +0300

    Added "Terms" page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -4,8 +4,7 @@ export const HOME_PAGE = '/';
 
 // External nav
 export const PARTNERS_LINK = '#partners';
-export const TERMS_LINK =
-  'https://drive.google.com/file/d/1J6A8f4VNYdL1SBz8xU42NzqMVH9l73PO/view?usp=drive_link';
+export const TERMS_LINK = '/terms';
 export const NODES_LINK = 'http://nodes.ringfence.ai';
 export const ESHER_LINK = 'http://escher.ringfence.ai';
 export const FOUNDATION_LINK = 'http://ringfence.org';
